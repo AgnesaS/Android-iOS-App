0ff0b50a51712825c42c28b2c207fa2302c828b4
diellzaaliu <58387674+diellzaaliu@users.noreply.github.com>
2021-06-21 13:59:48 +0200

    Update MainActivity.java
    Created the main layout

--- SEMESTRI VI/RestaurantMani/app/src/main/java/com/example/restaurantmani/MainActivity.java
+++ SEMESTRI VI/RestaurantMani/app/src/main/java/com/example/restaurantmani/MainActivity.java
@@ -1,14 +1,63 @@
-package com.example.restaurantmani;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login, signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.btnLogin);
+        signup = findViewById(R.id.signUpBtn);
+
+
+
+
+        //Transparent ActionBar
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+
+        );
+            getSupportActionBar().hide();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Sign_In_Activity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Sign_Up_Activity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
     }
 }
